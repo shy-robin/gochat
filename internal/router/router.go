b7230a1361af70b66fc8363e9ae88e91b3e540c9
shy-robin <shy_robin@163.com>
2025-11-19 11:00:42 +0800

    feat: mock api

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,39 @@ func NewRouter() *gin.Engine {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	ginServer.GET("/userInfo", func(ctx *gin.Context) {
+		userId := ctx.Query("id")
+		userName := ctx.Query("name")
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"userId":   userId,
+			"userName": userName,
+		})
+	})
+
+	ginServer.GET("/user/info/:id/:name", func(ctx *gin.Context) {
+		userId := ctx.Param("id")
+		userName := ctx.Param("name")
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"userId":   userId,
+			"userName": userName,
+		})
+	})
+
+	ginServer.POST("/json", func(ctx *gin.Context) {
+		data, _ := ctx.GetRawData()
+		var res map[string]any
+
+		_ = json.Unmarshal(data, &res)
+
+		ctx.JSON(http.StatusOK, res)
+	})
+
+	ginServer.GET("/redirect", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "https://www.baidu.com")
 	})
 
 	return ginServer
