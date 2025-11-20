ffdaaa65b6fbe4f82a6a699e3d2e082dcc74ca9b
shy-robin <shy_robin@163.com>
2025-11-20 15:48:15 +0800

    chore: 移除测试代码

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,11 +10,6 @@ func NewRouter() *gin.Engine {
 
 	{
 		group1 := ginServer.Group("/api/v1")
-		group1.GET("/ping", v1.Ping)
-		group1.GET("/userInfo", v1.GetUserInfo)
-		group1.GET("/user/info/:id/:name", v1.GetUserInfoRestful)
-		group1.POST("/json", v1.TestJson)
-		group1.GET("/redirect", v1.TestRedirect)
 
 		group1.POST("/user/register", v1.Register)
 	}
