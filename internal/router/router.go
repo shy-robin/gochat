be4ec16276276d4ec23117830c9b3c3d9d81d53c
shy-robin <shy_robin@163.com>
2025-11-23 18:19:53 +0800

    feat: CORS 解决跨域请求

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/shy-robin/gochat/config"
@@ -30,6 +31,8 @@ import (
 func NewRouter() *gin.Engine {
 	ginServer := gin.Default()
 
+	ginServer.Use(enableCORS())
+
 	{
 		group1 := ginServer.Group("/api/v1")
 
@@ -44,4 +47,42 @@ func NewRouter() *gin.Engine {
 	ginServer.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return ginServer
+}
+
+func enableCORS() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		method := ctx.Request.Method
+		origin := ctx.Request.Header.Get("Origin") //请求头部
+		if origin != "" {
+			ctx.Header(
+				"Access-Control-Allow-Origin",
+				"http://localhost:8083",
+			) // 可将将 * 替换为指定的域名
+			ctx.Header(
+				"Access-Control-Allow-Methods",
+				"POST,GET, OPTIONS, PUT, DELETE, UPDATE",
+			)
+			ctx.Header(
+				"Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept, Authorization",
+			)
+			ctx.Header(
+				"Access-Control-Expose-Headers",
+				"Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type",
+			)
+			ctx.Header(
+				"Access-Control-Allow-Credentials",
+				"true",
+			)
+		}
+
+		// 如果是预检请求 (OPTIONS)，直接返回 200/204 状态码
+		// 正确的预检响应：应该只有 CORS 头部，响应体为空，状态码为 200 OK 或 204 No Content
+		// 预检请求只是一个“问路”的请求，它不应该消耗任何业务资源
+		if method == "OPTIONS" {
+			ctx.JSON(http.StatusOK, "ok")
+		}
+
+		ctx.Next()
+	}
 }
